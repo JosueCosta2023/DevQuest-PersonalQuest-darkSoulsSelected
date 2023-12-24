fcf3c195460f7d65329e2d76fbde638d8c3e0f34
Josue Costa <contato_josuecosta@hotmail.com>
2023-12-24 15:24:50 -0400

    Create person

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+const personagens = document.querySelectorAll(".personagem");
+
+
+personagens.forEach((personagem)=>{
+    
+    personagem.addEventListener("mouseenter", ()=>{
+        
+        const idPersonagem = personagem.attributes.id.value;
+
+        const personagemSelecionado = document.querySelector(".selecionado");
+
+        personagemSelecionado.classList.remove("selecionado")
+
+        personagem.classList.add("selecionado");
+
+        const imagemGrande = document.getElementById("imagemGrandePersonagem");
+
+        imagemGrande.src=`../../assets/images/${idPersonagem}.png`;
+
+        const nomePersonagem = personagem.getAttribute('data-name');
+        const tituloPersonagem = personagem.getAttribute('data-title');
+        const biografiaPersonagem = personagem.getAttribute('data-history');
+
+        const nomePersonagemGrande = document.getElementById('nome')
+        const tituloPersonagemGrande = document.getElementById('titulo')
+        const biografiaPersonagemGrande = document.getElementById('biografia')
+
+        nomePersonagemGrande.innerHTML = nomePersonagem;
+        tituloPersonagemGrande.innerHTML = tituloPersonagem;
+        biografiaPersonagemGrande.innerHTML = biografiaPersonagem
+
+
+        })
+
+    })
